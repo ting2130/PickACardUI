54f5033107553332ec134a0eb18e65748a7046c1
ting2130 <142907352+ting2130@users.noreply.github.com>
2023-11-25 14:58:58 +0800

    加入專案檔案。

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for(int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + "of" + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 15);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1,5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Heart";
+            if (value == 3) return "Cluds";
+            return "Diamonds";
+        }
+    }
+}
